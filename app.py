da2fef015bfc949a8cf867d1072b95ff38a77991
soccer1220 <45838767+soccer1220@users.noreply.github.com>
2023-06-19 20:33:08 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,22 @@
 import streamlit as st
+import openai
+import re
+
+openai.api_key = st.secrets['api_key']
 
 st.write('# 형준 홈페이지')
-chart_data = [100, 200, 650]
+chart_data = [100, 150, 300, 600, 1400]
 st.bar_chart(chart_data)
+
+with st.form('name'):
+    name_input = st.text_input('이름을 입력하세요')
+    birth_input = st.text_input('생년월일(8자리)을 입력하세요')
+    user_sumit = st.form_submit_button('입력')
+    
+    if user_sumit: 
+        if name_input and birth_input:
+            st.write('환영합니다 {}님!'.format(name_input))
+        elif not name_input:
+            st.markdown(":red[이름을 다시 입력해주세요!]")
+        else:
+            st.markdown(":red[생년월일을 다시 입력해주세요!]")
